e7b279c40422620cc5348bcc75699fb82e7c5d4d
Adam Kapelner <kapelner@gmail.com>
2024-01-28 11:47:36 -0500

    gibbs and metropolis-within-gibbs demos for lec2

--- demos/lec02_hurdle_model.R
+++ demos/lec02_hurdle_model.R
@@ -2,7 +2,7 @@ pacman::p_load(ggplot2)
 set.seed(1)
 n = 50
 
-
+#############we don't get to see the real DGP!
 true_theta_1 = 0.5678
 true_theta_2 = 3.1415
 
@@ -14,19 +14,16 @@ for (i in 1 : n){
             rpois(1, true_theta_2) + 1
           }
 }
-n0 = sum(x == 0)
-n_minus_n0 = n - n0
-n0
-n_minus_n0
+#############we don't get to see the real DGP!
 
 ggplot(data.frame(x = x)) + 
   geom_histogram(aes(x = x))
 
 
 ##### now do the gibbs sampler
-total_samples = 1e4
-theta_1s = array(NA, total_samples)
-theta_2s = array(NA, total_samples)
+num_tot_samples = 1e4
+theta_1s = array(NA, num_tot_samples)
+theta_2s = array(NA, num_tot_samples)
 
 
 ###initialize thetas to best guesses
@@ -35,36 +32,42 @@ theta_1s[1]
 theta_2s[2] = mean(x[x > 0])
 theta_2s[2]
 
+##useful data to cache up top
+n0 = sum(x == 0)
+n_minus_n0 = n - n0
+n0
+n_minus_n0
 sum_x_minus_1_greater_than_0 = sum(x[x > 0] - 1)
 ###do the samples
-for (s in 2 : total_samples){
+for (s in 2 : num_tot_samples){
   theta_1s[s] = rbeta(1, n0 + 1, n_minus_n0)
   theta_2s[s] = rgamma(1, 1 + sum_x_minus_1_greater_than_0, n_minus_n0)
 }
 
+#collect all iterations
 gibbs_chain = data.frame(
   theta_1 = theta_1s, 
   theta_2 = theta_2s, 
-  t = 1 : total_samples
+  t = 1 : num_tot_samples
 )
+
+#assess convergence
 ggplot(gibbs_chain) +
   geom_point(aes(x = t, y = theta_1))
 ggplot(gibbs_chain) +
   geom_point(aes(x = t, y = theta_2))
 
 #burn in immediate, let's say to burn in something...
-burn_in = 100
-gibbs_chain = gibbs_chain[burn_in : total_samples, ]
+t_burn_in = 100
+gibbs_chain = gibbs_chain[t_burn_in : num_tot_samples, ]
 
 par(mfrow = c(2, 1))
-
 acf(gibbs_chain$theta_1)
 acf(gibbs_chain$theta_2)
 
 #no thin!
 
 #straight to inference
-
 ggplot(gibbs_chain) +
   geom_histogram(aes(x = theta_1)) +
   geom_vline(xintercept = mean(gibbs_chain$theta_1), col = "blue") + 
---
+++ demos/lec02_poisson_regression.R
@@ -0,0 +1,142 @@
+pacman::p_load(ggplot2)
+set.seed(1)
+n = 50
+
+#############we don't get to see the real DGP!
+true_theta_0 = 1
+true_theta_1 = 2
+t_time = seq(0, 5, length.out = n)
+y = array(NA, n)
+for (i in 1 : n){
+	y[i] = rpois(1, true_theta_0 + true_theta_1 * t_time[i])
+}
+#############we don't get to see the real DGP!
+
+ggplot(data.frame(t = t_time, y = y)) + 
+  geom_point(aes(x = t_time, y = y))
+
+
+#chains
+num_tot_samples = 1e5
+theta0s = array(NA, num_tot_samples)
+theta1s = array(NA, num_tot_samples)
+
+#start vals
+theta0s[1] = 1
+theta1s[1] = 1
+#hyperparams
+sigma_mh = 0.5 #MH sampling variance
+#diagnostic data (useful to collect)
+accept_theta0s = array(TRUE, num_tot_samples)
+accept_theta2s = array(TRUE, num_tot_samples)
+#functions that are useful
+ln_p_beta_0_beta_1_given_y_t_time = function(b0, b1){
+	sum(log(dpois(y, b0 + b1 * t_time)))
+}
+
+for (t in 2 : num_tot_samples){
+	theta0_t = theta0s[t - 1]
+	theta1_t = theta1s[t - 1]
+	
+	#sample beta_0 first
+	theta0star = rnorm(1, theta0_t, sigma_mh)
+	#calc r
+	ln_r = ln_p_beta_0_beta_1_given_y_t_time(theta0star, theta1_t) - 
+	  ln_p_beta_0_beta_1_given_y_t_time(theta0_t, theta1_t)
+	if (is.nan(ln_r) || (runif(1) > exp(ln_r))){
+		#reject - set it equal to previous value
+		theta0star = theta0_t
+		accept_theta0s[t] = FALSE
+	} #o/t accept
+	
+	#sample beta1 next
+	theta1star = rnorm(1, theta1_t, sigma_mh)
+	#calc r
+	ln_r = ln_p_beta_0_beta_1_given_y_t_time(theta0star, theta1star) - 
+	  ln_p_beta_0_beta_1_given_y_t_time(theta0star, theta1_t)
+	if (is.nan(ln_r) || (runif(1) > exp(ln_r))){
+		#reject
+		theta1star = theta1_t
+		accept_theta2s[t] = FALSE
+	} #o/t accept
+	
+	#record this iteration
+	theta0s[t] = theta0star
+	theta1s[t] = theta1star
+}
+
+#diagnostics - how often did the chain move?
+mean(accept_theta0s)
+mean(accept_theta2s)
+#not so great... we may want to go back and change our proposal distributions
+#if we have to thin too much
+
+#collect all iterations
+gibbs_chain = data.frame(
+  theta_0 = theta0s, 
+  theta_1 = theta1s, 
+  t = 1 : num_tot_samples
+)
+
+#assess convergence
+max_t_for_plotting = 2000
+ggplot(gibbs_chain) +
+  geom_point(aes(x = t, y = theta_0)) + 
+  xlim(0, max_t_for_plotting)
+ggplot(gibbs_chain) +
+  geom_point(aes(x = t, y = theta_1)) + 
+  xlim(0, max_t_for_plotting)
+
+#looks like it converged right away, let's burn a few just to be sure
+t_burn_in = 500
+
+#burn the chains
+theta0s = theta0s[t_burn_in : num_tot_samples]
+theta1s = theta1s[t_burn_in : num_tot_samples]
+
+##assess autocorrelation
+par(mfrow = c(2, 1))
+ell_max = 500
+r_max = 1
+acf(theta0s, 
+    xlim = c(0, ell_max + 10), ylim = c(0, r_max), lag.max = ell_max)
+acf(theta1s, 
+    xlim = c(0, ell_max + 10), ylim = c(0, r_max), lag.max = ell_max)
+#let's get a closer look to make sure by zooming in on the y-axis
+
+#let's make a judgment call - maybe at 100
+
+t_thin = 100
+
+#thin the chains
+gibbs_chain = gibbs_chain[seq(1, nrow(gibbs_chain), by = t_thin), ]
+
+
+#inference
+ggplot(gibbs_chain) +
+  geom_histogram(aes(x = theta_0)) +
+  geom_vline(xintercept = mean(gibbs_chain$theta_0), col = "blue") + 
+  geom_vline(xintercept = true_theta_0, col = "green") + 
+  geom_vline(xintercept = quantile(gibbs_chain$theta_0, .025), col = "red") + 
+  geom_vline(xintercept = quantile(gibbs_chain$theta_0, .975), col = "red")
+
+#mmse
+mean(gibbs_chain$theta_0)
+true_theta_0
+#CR_theta_0_95%
+c(quantile(gibbs_chain$theta_0, .025), quantile(gibbs_chain$theta_0, .975))
+
+ggplot(gibbs_chain) +
+  geom_histogram(aes(x = theta_1)) +
+  geom_vline(xintercept = mean(gibbs_chain$theta_1), col = "blue") + 
+  geom_vline(xintercept = true_theta_1, col = "green") + 
+  geom_vline(xintercept = quantile(gibbs_chain$theta_1, .025), col = "red") + 
+  geom_vline(xintercept = quantile(gibbs_chain$theta_1, .975), col = "red")
+
+#mmse
+mean(gibbs_chain$theta_1)
+true_theta_1
+#CR_theta_1_95%
+c(quantile(gibbs_chain$theta_1, .025), quantile(gibbs_chain$theta_1, .975))
+
+
